3c2d9ff34cffe38b604f941261a793533d727bd4
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2025-06-17 13:52:48 +0100

    feat: replace main menu title text with temporary title image

--- assets/menus/mainMenu/mainMenu.gd
+++ assets/menus/mainMenu/mainMenu.gd
@@ -2,7 +2,7 @@ extends OptionMenuBase
 
 func _ready() -> void:
 	super()
-	addCategory("Chicken Roll")
+	addImage("res://assets/menus/mainMenu/title.png")
 	#TODO playing the game and the main menu should all be within the same scene but its not for now
 	addButton("Play", func(): GameState.changeScene("res://scenes/game/game.tscn")); 
 	addButton("Settings", func(): onMenuEnter.emit("SettingsMenu"));
