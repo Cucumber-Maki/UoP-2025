ff5d8e28dcd6e9dd87c39da96c67decbeac4a677
Beth Cryer <52399621+beth-cryer@users.noreply.github.com>
2025-06-18 18:48:55 +0100

    add killplane to Game scene

--- assets/misc/respawn/kill_collider.gd
+++ assets/misc/respawn/kill_collider.gd
@@ -1,9 +1,15 @@
 extends Area3D
 
 @export var circle_wipe_animation: AnimationPlayer;
+var player: Player;
 
 func _ready() -> void:
 	self.body_entered.connect(_on_area_3d_body_entered);
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	circle_wipe_animation.play("circle_fade");
+	player = body as Player;
+	
+func respawn_player() -> void:
+	if (player == null): return;
+	player.respawn();
