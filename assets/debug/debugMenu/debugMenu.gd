f766ae7a0a30312b8b128070b15dcb0d3f181610
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2025-06-18 10:04:14 +0100

    feat: Chickkin debug menu setting.

--- assets/debug/debugMenu/debugMenu.gd
+++ assets/debug/debugMenu/debugMenu.gd
@@ -9,4 +9,5 @@ func _ready() -> void:
 	addCategory("Debug Settings");
 	addOption(OptionType.CheckBox, "Colliders Visible", "colliders_visible");
 	addOption(OptionType.CheckBox, "Wireframe Rendering", "render_wireframe");
+	addOption(OptionType.CheckBox, "Chickken Path Rendering", "render_chickinPaths");
 	addButton("Save & Exit", func(): onMenuExit.emit());
