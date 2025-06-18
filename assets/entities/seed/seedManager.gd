6410818de2190b5f9161b933a40af454de7efe82
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2025-06-18 17:12:22 +0100

    feat: add count of seeds for later use

--- assets/entities/seed/seedManager.gd
+++ assets/entities/seed/seedManager.gd
@@ -1,6 +1,11 @@
 extends Node
 
 var seedCount : int = 0;
+var totalSeeds : int;
+
+func _ready() -> void:
+	totalSeeds = get_children().size()
+
 
 func collectSeed() -> void:
 	print("Seed collected")
