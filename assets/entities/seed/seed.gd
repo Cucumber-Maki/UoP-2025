cdec713777f7d50a9d7486b5b5bc9b84445e3847
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2025-06-18 17:07:42 +0100

    feat: add scene manager to count seeds collected

--- assets/entities/seed/seed.gd
+++ assets/entities/seed/seed.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 signal seed_collected
 
+func _ready() -> void:
+	seed_collected.connect(get_parent().collectSeed)
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	var playerBody := body as Player
 	if playerBody != null:
---
+++ assets/entities/seed/seedManager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var seedCount : int = 0;
+
+func collectSeed() -> void:
+	print("Seed collected")
+	seedCount += 1
