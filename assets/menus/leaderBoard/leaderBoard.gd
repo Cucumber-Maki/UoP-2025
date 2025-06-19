8ad2aa9ad45d850fda0ab298c614a6df8bc82bfc
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2025-06-19 10:57:39 +0100

    refactor: Leaderboard grid container use.

--- assets/menus/leaderBoard/leaderBoard.gd
+++ assets/menus/leaderBoard/leaderBoard.gd
@@ -17,14 +17,35 @@ func _cleanMenu():
 	for child in children:
 		child.free()
 
+var m_activeLeaderboardContainer : GridContainer = null;
+func getLeaderboardContainer() -> GridContainer:
+	if (m_activeLeaderboardContainer == null):
+		m_activeLeaderboardContainer = GridContainer.new();
+		m_activeLeaderboardContainer.columns = 2;
+		getContentContainer().add_child(m_activeLeaderboardContainer);
+		#
+		var heading_name : Label = Label.new()
+		heading_name.text = "Name";
+		heading_name.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER;
+		m_activeLeaderboardContainer.add_child(heading_name);
+		var heading_seeds : Label = Label.new()
+		heading_seeds.text = "Seeds"
+		heading_name.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER;
+		m_activeLeaderboardContainer.add_child(heading_seeds);
+	return m_activeLeaderboardContainer;
+
 func _createMenu():
 	addCategory("Leader Board")
 	for score in m_scores:
-		var label : Label = Label.new()
-		label.text = "Name: {name}, Seeds: {seeds}".format(score)
-		label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		getContentContainer().add_child(label)
+		var content_name : Label = Label.new()
+		content_name.text = "{name}".format(score);
+		content_name.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER;
+		getLeaderboardContainer().add_child(content_name);
+		var content_seeds : Label = Label.new()
+		content_seeds.text = "{seeds}".format(score);
+		content_seeds.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER;
+		getLeaderboardContainer().add_child(content_seeds);
+		
 	addButton("Exit", func(): onMenuExit.emit());
 
 ################################################################################
