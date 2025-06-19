2e36fac50019cd945e2e688a598e2d0e6fa2a81f
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2025-06-19 15:55:37 +0100

    fix: fixes.

--- assets/audio/audio.gd
+++ assets/audio/audio.gd
@@ -17,16 +17,10 @@ func updateMusic() -> void:
 	updateGroup("AudioMusic", GameSettings.volume_master*GameSettings.volume_music);
 
 func updateGroup(group : StringName, volume : float) -> void:
-	print(volume)
 	var nodes := get_tree().get_nodes_in_group(group)
-	print(nodes)
 	for node in nodes:
 		var asp = node as AudioStreamPlayer
 		if asp == null:
 			asp = node as AudioStreamPlayer3D
-			print("Node is Audio3D")
-		
 		if asp != null:
 			asp.volume_db = linear_to_db(volume)
-			print("Volume set")
-		
